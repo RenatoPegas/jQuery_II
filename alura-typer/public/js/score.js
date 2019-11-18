ce132c5ad83a87f1adcfd597135bd2dde1c15c64
Renato <renato.note>
2019-11-18 16:38:45 -0300

    inicio dos estudos de animações

--- alura-typer/public/js/score.js
+++ alura-typer/public/js/score.js
@@ -1,3 +1,5 @@
+$("#btn-score").click(showScore);
+
 function scoreInsert(){
     let tableBody = $(".score").find("tbody");
     let user = "Renato";
@@ -6,7 +8,21 @@ function scoreInsert(){
     let tableLine = newLine(user, wordNumber);
     tableLine.find(".remove-buttom").click(removeLine);
  
-    tableBody.append(tableLine);                 
+    tableBody.append(tableLine); 
+
+    $(".score").slideDown(1000);         
+    scoreScroll();       
+ }
+
+ function scoreScroll(){
+    
+   let scorePosition = $(".score").offset().top;  //offset mostra a posição do placar]
+   $('body').animate(
+   {
+
+      scrollTop: scorePosition + 'px'
+
+   }, 1000);
  }
  
  function newLine(user, wordNumber){
@@ -31,7 +47,19 @@ function scoreInsert(){
     $(".remove-buttom").click( function(event){
  
        event.preventDefault();
-       $(this).parent().parent().remove();
- 
+       let line = $(this).parent().parent();
+       line.fadeOut(1000);                          //suaviza a sumida
+       
+       setTimeout(()=> {                             //executa função depois de 1 segundo
+         line.remove();
+       }, 1000)
+        
+
     });
+ }
+
+ function showScore(){
+   //  $(".score").toggle();
+    $(".score").stop().slideToggle(1000);   // mostra e esconde o placar com o suavidade 
+    
  }
