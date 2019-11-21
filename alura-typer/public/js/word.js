127be7aa7b9767218a060c6bd90658b458c371ed
Renato <renato.note>
2019-11-21 13:12:36 -0300

    teste

--- alura-typer/public/js/word.js
+++ alura-typer/public/js/word.js
@@ -1,13 +1,13 @@
 $("#btn-change").click(randomSentence);
 
 function randomSentence(){
-    $.get("http://localhost:3000/frases", changeRandomSentence);
+    $.get("http://localhost:3000/frases", changeRandomSentence);  //o comando get passa o endereço e uma função pra executar
 }
 
 function changeRandomSentence(data){
     
     let sentence = $(".sentence");
-    let randomNumber = Math.floor(Math.random() * data.length);
+    let randomNumber = Math.floor(Math.random() * data.length);   //para usar um numero aleatorio o Math.floor arrendonda o numero atravess de um aleatorio
     sentence.text(data[randomNumber].texto);
     updateSize();
     updateTime(data[randomNumber].tempo);
